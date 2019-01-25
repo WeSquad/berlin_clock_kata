bce57aca7c2cd120640f3a7ed35eeb9e0096d13a
sewelseb <sewelseb@hotmail.com>
2019-01-25 17:42:12 +0100

    project creation

---
+++ Berlin clock kata/Berlin clock kata/Converter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Berlin_clock_kata
+{
+    class Converter
+    {
+        public string ConvertTime(string time)
+        {
+            return "";
+        }
+    }
+}
---
+++ Berlin clock kata/Berlin clock kata/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Berlin_clock_kata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
