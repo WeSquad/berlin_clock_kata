4f792638493a818a7d2150ce7e1861e3611aecc1
sewelseb <sewelseb@hotmail.com>
2019-02-04 16:13:22 +0100

    SingleMinuteRow implemented

--- Berlin clock kata/Berlin clock kata/Converter.cs
+++ Berlin clock kata/Berlin clock kata/Converter.cs
@@ -1,4 +1,6 @@
-﻿namespace Berlin_clock_kata
+﻿using System.Linq;
+
+namespace Berlin_clock_kata
 {
     public class Converter
     {
@@ -9,26 +11,36 @@
 
         public string ConvertSingleMinutesRow(string time)
         {
+            var lightsOn = SelectSingleMinuteRowMinute(time);
+            var lightsOff = 4 - lightsOn;
+
+            return "Y".Repeat(lightsOn) + "O".Repeat(lightsOff);
+        }
+
+        private int SelectSingleMinuteRowMinute(string time)
+        {
             var minute = SelectMinute(time);
+            var simpleMinute = SimplifyMinute(minute);
+            return simpleMinute;
+        }
 
-            if (minute == 1)
-            {
-                return "YOOO";
-            }
-
-            if (minute == 2)
-            {
-                return "YYOO";
-            }
-
-            return "OOOO";
-
+        private int SimplifyMinute(int minute)
+        {
+            return minute % 5;
         }
 
         private int SelectMinute(string time)
         {
             var minute = time.Substring(4, 1);
             return int.Parse(minute);
+        }
+    }
+
+    public static class StringExtensions
+    {
+        public static string Repeat(this string item, int times)
+        {
+            return string.Join("", Enumerable.Repeat(item, times));
         }
     }
 }
